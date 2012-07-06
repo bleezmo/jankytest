f7b8ece99e69f967ba3f43502576592c761ad81e
Joshua Rogers <joshuarogers10@gmail.com>
2012-07-06 16:47:41 -0400

    internal server error

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,9 +6,8 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-	var x:Int = 5;
-	x = x / 0;
-    Ok(views.html.index("Your new application is ready. go fuck yourself"))
+    //Ok(views.html.index("Your new application is ready. go fuck yourself"))
+	InternalServerError("fuck you")
   }
   
 }
