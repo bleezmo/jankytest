90a7b1d16e882d2877d3784357b6c98861823256
Joshua Rogers <joshuarogers10@gmail.com>
2012-07-17 11:24:11 -0400

    fixed

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,8 +6,6 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Logger.error("shiiiiit")
-    val x = 1/ 0;
     Ok(views.html.index("Your new application is ready. go fuck yourself"))
   }
   
